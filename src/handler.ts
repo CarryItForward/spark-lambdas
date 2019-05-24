71b77f2f2c9f24430fb0a776f917e5d2ddf7c5a0
Andrew Dassonville <dassonville.andrew@gmail.com>
2019-05-24 11:24:52 -0700

    Test path parameters

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,6 @@
 import { successResponse } from './utils'
 
 export const hello = async (event: AWSLambda.APIGatewayEvent) => successResponse({
-  message: 'Go Serverless v1.0! Your function executed successfully!',
+  message: `Hello, ${event.pathParameters ? event.pathParameters.name : ''}`,
   input: event,
 })
